2736feb5b2372f66c98b711886dca6d4cb3022cf
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-02-10 21:28:07 -0500

    add routes and header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                "kape-green": "#4b6158",
+                "kape-beige": "#eeebe2",
+            },
+        },
+    },
+    plugins: [],
+};
