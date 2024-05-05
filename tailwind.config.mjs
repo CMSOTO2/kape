a3df0b6715dd749b1d05d741cc221a03b38c1ce6
CMSOTO2 <carlos.m.soto2@gmail.com>
2024-05-04 21:48:36 -0400

    follow us section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,8 @@ export default {
                 "kape-green": "#4b6158",
                 "kape-beige": "#eeebe2",
                 "kape-black": "#0A0A0A",
+                "kape-gray": "#242424",
+                "kape-orange": "#F4A259"
             },
             fontFamily: {
                 sans: ['"Roboto"', ...defaultTheme.fontFamily.sans],
